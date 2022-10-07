f5b5170ca2e4ac549124fcdfd5129ff1fa52c2ae
onTDB <github@ontdb.com>
2022-10-07 21:55:00 +0900

    Update when cache deleted

--- app.py
+++ app.py
@@ -10,17 +10,20 @@ CORS(app)
 
 @app.route('/<path:path>')
 def static_file(path):
+    if path == "favicon.ico": return {"status": 404}, 404
+    os.chdir(Path(__file__).parent)
     img_id = path.split('.')[0]
     cache_folder = Path('cache')
     if not cache_folder.exists(): cache_folder.mkdir()
     cache_file = cache_folder / f'{img_id}.png'
-    if cache_file.exists(): return cache_file.read_bytes()
     try:
         if img_id in requests.get('http://localhost:5000/api/ddr_imglist').json()["data"]:
+            if cache_file.exists(): return send_file(cache_file)
             img = requests.get('http://localhost:5000/api/ddr_img?id=' + img_id).content
             cache_file.write_bytes(img)
-            return img
+            return send_file(cache_file)
         else:
+            if cache_file.exists(): cache_file.unlink()
             return {"status": 404, "message": "Image not found"}
     except:
         return {"status": 500, "message": "Internal Server Error"}
